f065412359c8fe9fdfae226702ea598606592f97
gtmylab <75452117+gtmylab@users.noreply.github.com>
2020-12-04 21:42:18 +0100

    Update vesta-install.sh

--- vesta-install.sh
+++ vesta-install.sh
@@ -1,7 +1,8 @@
 # Download installation script
 curl -O http://vestacp.com/pub/vst-install.sh
 # Run it
-bash vst-install.sh --nginx no --apache yes --phpfpm no --named yes --remi yes --vsftpd no --proftpd yes --iptables no --fail2ban no --quota yes --exim yes --dovecot yes --spamassassin no --clamav no --softaculous yes --mysql yes --postgresql no
+bash vst-install.sh --nginx no --apache yes --phpfpm no --named yes --remi yes --vsftpd no --proftpd yes --iptables no --fail2ban no --quota yes --exim yes --dovecot yes --spamassassin no --clamav no --softaculous yes --mysql yes --postgresql no --password admin25
+
 
 #Run PHP Modified
 curl -O https://raw.githubusercontent.com/gtmylab/vcp/main/vesta-php.sh
