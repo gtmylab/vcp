a67653d3cd17c8e72fa080014a3e2cc0b4ff876b
gtmylab <75452117+gtmylab@users.noreply.github.com>
2024-06-08 06:09:31 +0800

    Create vesta-mail-install.sh

---
+++ vesta-mail-install.sh
@@ -0,0 +1,9 @@
+# Download installation script
+curl -O http://vestacp.com/pub/vst-install.sh
+# Run it
+bash vst-install.sh --nginx yes --apache yes --phpfpm no --named no --remi yes --vsftpd no --proftpd yes --iptables no --fail2ban no --quota no --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --email g_tmy@hotmail.com --password admin25
+
+#Run Roundcube Installer
+curl -O https://raw.githubusercontent.com/gtmylab/vcp/main/install_roundcube.sh
+chmod +x install_roundcube.sh
+./install_roundcube.sh
