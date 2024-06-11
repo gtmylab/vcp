3938fdeca911dd3b7228d02ce525bda44d8fa0f5
gtmylab <75452117+gtmylab@users.noreply.github.com>
2024-06-12 04:36:37 +0800

    Update vesta-mail-install.sh

--- vesta-mail-install.sh
+++ vesta-mail-install.sh
@@ -1,8 +1,7 @@
 # Download installation script
 curl -O http://vestacp.com/pub/vst-install.sh
 # Run it
-bash vst-install.sh --nginx yes --apache yes --phpfpm no --named no --remi yes --vsftpd no --proftpd yes --iptables no --fail2ban no --quota no --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --email g_tmy@hotmail.com --password admin25
-
+bash vst-install.sh --nginx yes --apache yes --phpfpm no --named yes --remi yes --vsftpd no --proftpd yes --iptables no --fail2ban no --quota yes --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --email g_tmy@hotmail.com --password admin25
 #Run Roundcube Installer
 curl -O https://raw.githubusercontent.com/gtmylab/vcp/main/install_roundcube.sh
 chmod +x install_roundcube.sh
